e33035c1146013fee51cf9144eabbc8ff78649ec
Sianooo <117085166+Sianooo@users.noreply.github.com>
2024-10-07 23:44:44 +0200

    Add files via upload

---
+++ JSON,AJAX,API/script.js
@@ -0,0 +1,22 @@
+// const btn=document.querySelector('button')
+// const img=document.querySelector('img')
+
+// const URL='https://dog.ceo/api/breeds/image/random'
+
+//fetch().then().catch()
+// fetch(URL)
+// .then(res=> console.log(res))
+// .catch(err=>console.error(err))
+
+// fetch(URL)
+// .then(res=> res.json())
+// .then(data=>console.log(data))
+// .catch(err=>console.error(err))
+
+// btn.addEventListener('click',()=>{
+//     fetch(URL)
+//     .then(res=> res.json())
+//     .then(data=>img.setAttribute('src',data.message))
+//     .catch(err=>console.error(err))
+    
+// })
